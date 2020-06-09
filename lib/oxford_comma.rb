7a8600ebb416b2a6d996724043513f1419c250b3
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-09 13:27:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2 
     array.join (" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
     final_string
     array.each do |index|
       if array.equal? array.last
